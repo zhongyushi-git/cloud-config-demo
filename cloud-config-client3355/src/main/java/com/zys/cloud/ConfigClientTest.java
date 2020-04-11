ea2e2fc84b2ed73a2299c47864d5f3c8a1994646
zhongyushi-git <1916008067@qq.com>
2020-04-11 17:17:28 +0800

    添加bus

--- cloud-config-client3355/src/main/java/com/zys/cloud/ConfigClientTest.java
+++ cloud-config-client3355/src/main/java/com/zys/cloud/ConfigClientTest.java
@@ -1,10 +1,12 @@
 package com.zys.cloud;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class ConfigClientTest {
 
     @Value("${spring.application.name}")
@@ -14,9 +16,18 @@ public class ConfigClientTest {
     @Value("${server.port}")
     private String serverPort;
 
+    @Value("${config.info}")
+    private String info;
+
     @GetMapping("/config")
     public String test(){
         String str="applicationName: "+applicationName+"\t"+"serverPort:"+serverPort;
+        return str;
+    }
+
+    @GetMapping("/config2")
+    public String test2(){
+        String str="info: "+info;
         return str;
     }
 
